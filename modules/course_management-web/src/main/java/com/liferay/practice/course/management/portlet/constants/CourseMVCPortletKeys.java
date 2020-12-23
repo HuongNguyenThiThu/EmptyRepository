cad80f6d228fa8c3414501dba121995af3f45797
HuongNguyen-gs <huong.nguyen@gs.liferay.com>
2020-12-23 16:20:04 +0700

    created portlet with service builder

---
+++ modules/course_management-web/src/main/java/com/liferay/practice/course/management/portlet/constants/CourseMVCPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.practice.course.management.portlet.constants;
+
+/**
+ * @author ces-user
+ */
+public class CourseMVCPortletKeys {
+
+	public static final String COURSEMVC =
+		"com_liferay_practice_course_management_portlet_CourseMVCPortlet";
+
+}
